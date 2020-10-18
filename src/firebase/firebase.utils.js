324c87ac490c50df3964616809e78885492bf5fb
binukv1989 <kvbinuthilak@gmail.com>
2020-10-18 21:57:18 +0530

    implemented sign up with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ const config = {
     appId: "1:727851331490:web:1fa24ab0d6b942b8d24cfb"
   };
 
+    export const createUserProfileDocument = async(userAuth, additionalData)=>{
+        if(!userAuth) return;
+
+        const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+        const snapShot = await userRef.get();
+
+          if(!snapShot.exists){
+              const { displayName,email } = userAuth;
+              const createdAt = new Date();
+
+              try{
+                  await userRef.set({
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+                  });
+              }catch(error){
+                  console.log('error created by user',error.message);
+              }
+          }
+          return userRef;
+    };
+
+
+
   firebase.initializeApp(config);
 
 
--- src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import SignIn from '../../component/sign-in/sign-in.component';
 
+import SignUP from '../../component/sign-up/sign-up.component';
+
 import './sign-in-and-sign-up-page.styles.scss'
 
 
 const SignInAndSignUp = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUP/>
     </div>
 );
 
